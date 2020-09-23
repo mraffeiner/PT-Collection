6089d5210e84964ab22794e73add06b02cd2205d
Matthias <matthias.raffeiner@student.uni-tuebingen.de>
2020-09-23 12:01:25 +0200

    Enemy Pathing, Test Map, Animations
    - Added pathing scripts to enemy to follow player
    - Created initial map with 4 exits for testing, exits also have a trigger box to prevent enemies from pathing through
    - Copies enemy animation from tower defense and adjusted it to fit in twin stick

---
+++ Assets/_Main/Games/Twin Stick Shooter/Scripts/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Pathfinding;
+
+namespace PTCollection.TwinStickShooter
+{
+    public class Enemy : MonoBehaviour
+    {
+        [SerializeField] private CanvasGroup healthbarCanvasGroup = null;
+        [SerializeField] private Image healthbarFillImage = null;
+        [SerializeField] private float moveSpeed = 0f;
+
+        private AIPath aiPath;
+
+        private void Awake() => aiPath = GetComponent<AIPath>();
+
+        private void Start() => aiPath.maxSpeed = moveSpeed;
+    }
+}
